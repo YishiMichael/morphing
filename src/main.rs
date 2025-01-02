189d1ff89d588f5dd20b9bc2160fee09b26eeec4
YishiMichael <50232075+YishiMichael@users.noreply.github.com>
2025-01-02 04:18:04 -0500

    feat: frame extraction

--- src/main.rs
+++ src/main.rs
@@ -4,92 +4,76 @@ pub mod path;
 pub mod stroke;
 pub mod world;
 
-use fill::Fill;
+use std::borrow::Cow;
+
 use itertools::Itertools;
+
+use fill::Fill;
 use mobject::Mobject;
-use path::ManipulatorGroupId;
 use path::Path;
-use path::Subpaths;
+use path::PathBuilder;
 use stroke::Stroke;
+use ttf_parser::OutlineBuilder;
 use world::TypstWorld;
 
-fn typst_path_to_subpath(path: typst::visualize::Path) -> bezier_rs::Subpath<ManipulatorGroupId> {
-    #[inline]
-    fn convert_point(typst::layout::Point { x, y }: typst::layout::Point) -> (f64, f64) {
-        (x.to_raw(), y.to_raw())
-    }
-
-    let mut anchor = None;
-    let mut closed = false;
-    let beziers = path
-        .0
-        .into_iter()
-        .filter_map(|path_item| match path_item {
-            typst::visualize::PathItem::MoveTo(start) => {
-                assert!(anchor.replace(convert_point(start)).is_none());
-                None
-            }
-            typst::visualize::PathItem::LineTo(end) => {
-                let end = convert_point(end);
-                Some(bezier_rs::Bezier {
-                    start: anchor.replace(end).unwrap().into(),
-                    end: end.into(),
-                    handles: bezier_rs::BezierHandles::Linear,
-                })
-            }
-            typst::visualize::PathItem::CubicTo(handle_start, handle_end, end) => {
-                let end = convert_point(end);
-                Some(bezier_rs::Bezier {
-                    start: anchor.replace(end).unwrap().into(),
-                    end: end.into(),
-                    handles: bezier_rs::BezierHandles::Cubic {
-                        handle_start: convert_point(handle_start).into(),
-                        handle_end: convert_point(handle_end).into(),
-                    },
-                })
-            }
-            typst::visualize::PathItem::ClosePath => {
-                assert!(anchor.take().is_some());
-                closed = true;
-                None
-            }
-        })
-        .collect_vec();
-    bezier_rs::Subpath::from_beziers(&beziers, closed)
+fn typst_path_to_path(path: &typst::visualize::Path) -> Path {
+    let mut builder = PathBuilder::new();
+    path.0.iter().for_each(|path_item| match path_item {
+        &typst::visualize::PathItem::MoveTo(start) => {
+            builder.move_to(start.x.to_pt() as f32, start.y.to_pt() as f32)
+        }
+        &typst::visualize::PathItem::LineTo(end) => {
+            builder.line_to(end.x.to_pt() as f32, end.y.to_pt() as f32)
+        }
+        &typst::visualize::PathItem::CubicTo(handle_start, handle_end, end) => builder.curve_to(
+            handle_start.x.to_pt() as f32,
+            handle_start.y.to_pt() as f32,
+            handle_end.x.to_pt() as f32,
+            handle_end.y.to_pt() as f32,
+            end.x.to_pt() as f32,
+            end.y.to_pt() as f32,
+        ),
+        &typst::visualize::PathItem::ClosePath => builder.close(),
+    });
+    builder.build()
 }
 
-pub fn typst_shape_to_mobjects(shape: typst::visualize::Shape) -> Vec<Box<dyn Mobject>> {
-    let subpath = typst_path_to_subpath(match shape.geometry {
-        typst::visualize::Geometry::Line(point) => {
-            let mut path = typst::visualize::Path::new();
-            path.line_to(point);
-            path
-        }
-        typst::visualize::Geometry::Rect(size) => typst::visualize::Path::rect(size),
-        typst::visualize::Geometry::Path(path) => path,
-    });
+fn outline_glyph_to_path(font: &typst::text::Font, id: ttf_parser::GlyphId) -> Option<Path> {
+    let mut builder = PathBuilder::new();
+    font.ttf().outline_glyph(id, &mut builder)?;
+    Some(builder.build())
+}
 
-    let mut mobjects: Vec<Box<dyn Mobject>> = Vec::new();
-    if let Some(fill) = shape.fill {
-        if let typst::visualize::Paint::Solid(color) = fill {
-            mobjects.push(Box::new(Fill {
-                path: Path::from(Subpaths::from(subpath.clone())),
-                color: rgb::Rgba::from(color.to_vec4()),
-                options: match shape.fill_rule {
-                    typst::visualize::FillRule::NonZero => {
-                        lyon::tessellation::FillOptions::non_zero()
-                    }
-                    typst::visualize::FillRule::EvenOdd => {
-                        lyon::tessellation::FillOptions::even_odd()
-                    }
-                },
-            }));
+fn path_to_mobjects(
+    path: Path,
+    fill_rule: Option<typst::visualize::FillRule>,
+    fill: Option<&typst::visualize::Paint>,
+    stroke: Option<&typst::visualize::FixedStroke>,
+) -> Vec<Box<dyn Mobject>> {
+    #[inline]
+    fn paint_to_color(paint: &typst::visualize::Paint) -> rgb::Rgba<f32> {
+        if let typst::visualize::Paint::Solid(color) = paint {
+            rgb::Rgba::from(color.to_vec4())
         } else {
             panic!("Unsopported paint");
         }
     }
-    if let Some(stroke) = shape.stroke {
-        if let typst::visualize::Paint::Solid(color) = stroke.paint {
+
+    let stroke_mobject = stroke.map(|stroke| Stroke {
+        path: if let Some(dash_pattern) = &stroke.dash {
+            path.dash(
+                &dash_pattern
+                    .array
+                    .iter()
+                    .map(|length| length.to_pt())
+                    .collect_vec(),
+                dash_pattern.phase.to_pt(),
+            )
+        } else {
+            path.clone()
+        },
+        color: paint_to_color(&stroke.paint),
+        options: {
             let cap = match stroke.cap {
                 typst::visualize::LineCap::Butt => lyon::tessellation::LineCap::Butt,
                 typst::visualize::LineCap::Round => lyon::tessellation::LineCap::Round,
@@ -100,62 +84,165 @@ pub fn typst_shape_to_mobjects(shape: typst::visualize::Shape) -> Vec<Box<dyn Mo
                 typst::visualize::LineJoin::Round => lyon::tessellation::LineJoin::Round,
                 typst::visualize::LineJoin::Bevel => lyon::tessellation::LineJoin::Bevel,
             };
-            mobjects.push(Box::new(Stroke {
-                path: if let Some(dash_pattern) = stroke.dash {
-                    let total_length = subpath.length(None);
-                    let phase = dash_pattern.phase.to_raw() / total_length;
-                    let mut alphas = dash_pattern
-                        .array
-                        .into_iter()
-                        .map(|length| length.to_raw() / total_length)
-                        .scan(0.0, |alpha_acc, alpha| {
-                            *alpha_acc += alpha;
-                            Some(*alpha_acc)
-                        })
-                        .collect_vec();
-                    alphas.rotate_right(1);
-                    let alpha_period = alphas
-                        .get_mut(0)
-                        .map(|alpha| std::mem::take(alpha))
-                        .unwrap_or_default();
-                    Path::from(Subpaths(
-                        (-(phase / alpha_period).ceil() as i32
-                            ..((1.0 - phase) / alpha_period).ceil() as i32)
-                            .flat_map(|i| {
-                                alphas
-                                    .iter()
-                                    .tuples()
-                                    .filter_map(move |(&alpha_0, &alpha_1)| {
-                                        let alpha_0 = (i as f64 * alpha_period + alpha_0).max(0.0);
-                                        let alpha_1 = (i as f64 * alpha_period + alpha_1).min(1.0);
-                                        (alpha_0 < alpha_1).then_some((alpha_0, alpha_1))
-                                    })
-                            })
-                            .map(|(alpha_0, alpha_1)| {
-                                subpath.trim(
-                                    bezier_rs::SubpathTValue::GlobalEuclidean(alpha_0),
-                                    bezier_rs::SubpathTValue::GlobalEuclidean(alpha_1),
-                                )
-                            })
-                            .collect(),
-                    ))
-                } else {
-                    Path::from(Subpaths::from(subpath))
-                },
-                color: rgb::Rgba::from(color.to_vec4()),
-                options: lyon::tessellation::StrokeOptions::default()
-                    .with_line_width(stroke.thickness.to_raw() as f32)
-                    .with_start_cap(cap)
-                    .with_end_cap(cap)
-                    .with_line_join(join)
-                    .with_miter_limit(stroke.miter_limit.get() as f32),
-            }));
-        } else {
-            panic!("Unsopported paint");
-        }
-    }
+            lyon::tessellation::StrokeOptions::default()
+                .with_line_width(stroke.thickness.to_pt() as f32)
+                .with_start_cap(cap)
+                .with_end_cap(cap)
+                .with_line_join(join)
+                .with_miter_limit(stroke.miter_limit.get() as f32)
+        },
+    });
+    let fill_mobject = fill.map(|fill| Fill {
+        path,
+        color: paint_to_color(fill),
+        options: match fill_rule.unwrap_or_default() {
+            typst::visualize::FillRule::NonZero => lyon::tessellation::FillOptions::non_zero(),
+            typst::visualize::FillRule::EvenOdd => lyon::tessellation::FillOptions::even_odd(),
+        },
+    });
 
+    let mut mobjects: Vec<Box<dyn Mobject>> = Vec::new();
+    if let Some(fill_mobject) = fill_mobject {
+        mobjects.push(Box::new(fill_mobject));
+    }
+    if let Some(stroke_mobject) = stroke_mobject {
+        mobjects.push(Box::new(stroke_mobject));
+    }
     mobjects
+}
+
+fn typst_shape_to_mobjects(
+    shape: &typst::visualize::Shape,
+    span: typst::syntax::Span,
+    transform: typst::layout::Transform,
+) -> Vec<(
+    typst::syntax::Span,
+    Box<dyn Mobject>,
+    typst::layout::Transform,
+)> {
+    let typst_path = match &shape.geometry {
+        &typst::visualize::Geometry::Line(point) => {
+            let mut path = typst::visualize::Path::new();
+            path.line_to(point);
+            Cow::Owned(path)
+        }
+        &typst::visualize::Geometry::Rect(size) => Cow::Owned(typst::visualize::Path::rect(size)),
+        &typst::visualize::Geometry::Path(ref path) => Cow::Borrowed(path),
+    };
+    path_to_mobjects(
+        typst_path_to_path(&typst_path),
+        Some(shape.fill_rule),
+        shape.fill.as_ref(),
+        shape.stroke.as_ref(),
+    )
+    .into_iter()
+    .map(|mobject| (span, mobject, transform))
+    .collect()
+}
+
+fn typst_text_to_mobjects(
+    text: &typst::text::TextItem,
+    transform: typst::layout::Transform,
+) -> Vec<(
+    typst::syntax::Span,
+    Box<dyn Mobject>,
+    typst::layout::Transform,
+)> {
+    let scale = typst::layout::Ratio::new(text.size.to_pt() / text.font.units_per_em());
+    text.glyphs
+        .iter()
+        .scan(typst::layout::Abs::pt(0.0), |x, glyph| {
+            let offset = *x + glyph.x_offset.at(text.size);
+            *x += glyph.x_advance.at(text.size);
+
+            let transform = transform
+                .pre_concat(typst::layout::Transform::scale(
+                    typst::layout::Ratio::one(),
+                    -typst::layout::Ratio::one(),
+                ))
+                .pre_concat(typst::layout::Transform::translate(
+                    offset,
+                    typst::layout::Abs::zero(),
+                ))
+                .pre_concat(typst::layout::Transform::scale(scale, scale));
+
+            Some((transform, glyph))
+        })
+        .flat_map(|(transform, glyph)| {
+            outline_glyph_to_path(&text.font, ttf_parser::GlyphId(glyph.id))
+                .into_iter()
+                .flat_map(move |path| {
+                    path_to_mobjects(path, None, Some(&text.fill), text.stroke.as_ref())
+                        .into_iter()
+                        .map(
+                            move |mobject| (glyph.span.0, mobject, transform), // TODO: option span?)
+                        )
+                })
+        })
+        .collect()
+}
+
+pub fn typst_frame_to_mobjects(
+    frame: &typst::layout::Frame,
+    transform: typst::layout::Transform,
+) -> Vec<(
+    typst::syntax::Span,
+    Box<dyn Mobject>,
+    typst::layout::Transform,
+)> {
+    // #[inline]
+    // fn convert_point(typst::layout::Point { x, y }: typst::layout::Point) -> (f32, f32) {
+    //     (x.to_pt() as f32, y.to_pt() as f32)
+    // }
+
+    // #[inline]
+    // fn convert_transform(
+    //     typst::layout::Transform {
+    //         sx,
+    //         ky,
+    //         kx,
+    //         sy,
+    //         tx,
+    //         ty,
+    //     }: typst::layout::Transform,
+    // ) -> ttf_parser::Transform {
+    //     ttf_parser::Transform::new(
+    //         sx.get() as f32,
+    //         ky.get() as f32,
+    //         kx.get() as f32,
+    //         sy.get() as f32,
+    //         tx.to_pt() as f32,
+    //         ty.to_pt() as f32,
+    //     )
+    // }
+
+    frame
+        .items()
+        .flat_map(|(position, item)| {
+            // let (x, y) = convert_point(*position);
+            let transform =
+                transform.pre_concat(typst::layout::Transform::translate(position.x, position.y));
+            match item {
+                &typst::layout::FrameItem::Group(ref group) => {
+                    if group.clip_path.is_some() {
+                        panic!("Clip path not supported");
+                    }
+                    typst_frame_to_mobjects(&group.frame, transform.pre_concat(group.transform))
+                }
+                &typst::layout::FrameItem::Text(ref text) => {
+                    typst_text_to_mobjects(text, transform)
+                }
+                &typst::layout::FrameItem::Shape(ref shape, span) => {
+                    typst_shape_to_mobjects(shape, span, transform)
+                }
+                &typst::layout::FrameItem::Image(_, _, _) => {
+                    panic!("Unsopported item: image")
+                }
+                &typst::layout::FrameItem::Link(..) => panic!("Unsopported item: link"),
+                &typst::layout::FrameItem::Tag(..) => panic!("Unsopported item: tag"),
+            }
+        })
+        .collect()
 }
 
 // fn visit_frame_items<F: FnMut(typst::layout::FrameItem, typst::layout::Transform)>(
@@ -197,6 +284,8 @@ fn main() -> () {
     let document = world.document(text);
 
     let frame = document.pages.into_iter().exactly_one().unwrap().frame; // TODO: unwrap
+
+    dbg!(typst_frame_to_mobjects(&frame, typst::layout::Transform::identity()).len());
 
     // frame.items().for_each(|a|);
 
--- src/path.rs
+++ src/path.rs
@@ -1,79 +1,68 @@
 use itertools::Itertools;
 
-#[derive(Clone)]
-pub struct Path(pub lyon::path::Path);
+// #[derive(Clone)]
+// pub struct Path(pub lyon::path::Path);
 
 #[derive(Clone)]
-pub struct Subpaths(pub Vec<bezier_rs::Subpath<ManipulatorGroupId>>);
+pub struct Path(Vec<bezier_rs::Subpath<ManipulatorGroupId>>);
 
-impl From<&Subpaths> for Path {
-    fn from(subpaths: &Subpaths) -> Self {
-        #[inline]
-        fn convert_point((x, y): (f64, f64)) -> lyon::geom::Point<f32> {
-            lyon::geom::Point::new(x as f32, y as f32)
-        }
+// TODO: port ctors from bezier_rs::Subpath
 
-        Self(lyon::path::Path::from_iter(subpaths.0.iter().flat_map(
-            |subpath| {
-                let begin_point = convert_point(subpath[0].anchor.into());
-                let end_point = convert_point(subpath[subpath.len() - 1].anchor.into());
-                std::iter::once(lyon::path::PathEvent::Begin { at: begin_point })
-                    .chain(subpath.iter().map(
-                        |bezier_rs::Bezier {
-                             start: from,
-                             end: to,
-                             handles,
-                         }| {
-                            match handles {
-                                bezier_rs::BezierHandles::Linear => lyon::path::PathEvent::Line {
-                                    from: convert_point(from.into()),
-                                    to: convert_point(to.into()),
-                                },
-                                bezier_rs::BezierHandles::Quadratic { handle: ctrl } => {
-                                    lyon::path::PathEvent::Quadratic {
-                                        from: convert_point(from.into()),
-                                        ctrl: convert_point(ctrl.into()),
-                                        to: convert_point(to.into()),
-                                    }
-                                }
-                                bezier_rs::BezierHandles::Cubic {
-                                    handle_start: ctrl1,
-                                    handle_end: ctrl2,
-                                } => lyon::path::PathEvent::Cubic {
-                                    from: convert_point(from.into()),
-                                    ctrl1: convert_point(ctrl1.into()),
-                                    ctrl2: convert_point(ctrl2.into()),
-                                    to: convert_point(to.into()),
-                                },
-                            }
-                        },
-                    ))
-                    .chain(std::iter::once(lyon::path::PathEvent::End {
-                        last: end_point,
-                        first: begin_point,
-                        close: subpath.closed,
-                    }))
-                    .collect_vec()
-            },
-        )))
+impl Path {
+    pub fn concat<I: IntoIterator<Item = Self>>(iter: I) -> Self {
+        Self::from_iter(iter.into_iter().flat_map(|path| path.0))
     }
-}
 
-impl From<Subpaths> for Path {
-    #[inline]
-    fn from(subpaths: Subpaths) -> Self {
-        Self::from(&subpaths)
+    pub fn subpaths(&self) -> &[bezier_rs::Subpath<ManipulatorGroupId>] {
+        &self.0
     }
-}
 
-impl From<&Path> for Subpaths {
-    fn from(path: &Path) -> Self {
+    pub fn dash(&self, array: &[f64], phase: f64) -> Self {
+        Self::from_iter(self.subpaths().into_iter().flat_map(|subpath| {
+            let total_length = subpath.length(None);
+            let phase = phase / total_length;
+            let mut alphas = array
+                .into_iter()
+                .map(|length| length / total_length)
+                .scan(0.0, |alpha_acc, alpha| {
+                    *alpha_acc += alpha;
+                    Some(*alpha_acc)
+                })
+                .collect_vec();
+            alphas.rotate_right(1);
+            let alpha_period = alphas
+                .get_mut(0)
+                .map(|alpha| std::mem::take(alpha))
+                .unwrap_or_default();
+            (-(phase / alpha_period).ceil() as i32..((1.0 - phase) / alpha_period).ceil() as i32)
+                .map(move |i| i as f64 * alpha_period)
+                .flat_map(move |alpha_offset| {
+                    alphas
+                        .clone()
+                        .into_iter()
+                        .tuples()
+                        .filter_map(move |(alpha_0, alpha_1)| {
+                            let alpha_0 = (alpha_offset + alpha_0).max(0.0);
+                            let alpha_1 = (alpha_offset + alpha_1).min(1.0);
+                            (alpha_0 < alpha_1).then_some((alpha_0, alpha_1))
+                        })
+                })
+                .map(|(alpha_0, alpha_1)| {
+                    subpath.trim(
+                        bezier_rs::SubpathTValue::GlobalEuclidean(alpha_0),
+                        bezier_rs::SubpathTValue::GlobalEuclidean(alpha_1),
+                    )
+                })
+        }))
+    }
+
+    pub fn from_lyon_path(path: &lyon::path::Path) -> Self {
         #[inline]
         fn convert_point(lyon::geom::Point { x, y, .. }: lyon::geom::Point<f32>) -> (f64, f64) {
             (x as f64, y as f64)
         }
 
-        let mut event_iter = path.0.iter();
+        let mut event_iter = path.iter();
         let mut subpaths = Vec::new();
         while event_iter.next().is_some() {
             let beziers = event_iter
@@ -122,19 +111,116 @@ impl From<&Path> for Subpaths {
         }
         Self(subpaths)
     }
-}
 
-impl From<Path> for Subpaths {
-    #[inline]
-    fn from(path: Path) -> Self {
-        Self::from(&path)
+    pub fn to_lyon_path(&self) -> lyon::path::Path {
+        #[inline]
+        fn convert_point((x, y): (f64, f64)) -> lyon::geom::Point<f32> {
+            lyon::geom::point(x as f32, y as f32)
+        }
+
+        lyon::path::Path::from_iter(self.subpaths().iter().flat_map(|subpath| {
+            let begin_point = convert_point(subpath[0].anchor.into());
+            let end_point = convert_point(subpath[subpath.len() - 1].anchor.into());
+            std::iter::once(lyon::path::PathEvent::Begin { at: begin_point })
+                .chain(subpath.iter().map(
+                    |bezier_rs::Bezier {
+                         start: from,
+                         end: to,
+                         handles,
+                     }| {
+                        match handles {
+                            bezier_rs::BezierHandles::Linear => lyon::path::PathEvent::Line {
+                                from: convert_point(from.into()),
+                                to: convert_point(to.into()),
+                            },
+                            bezier_rs::BezierHandles::Quadratic { handle: ctrl } => {
+                                lyon::path::PathEvent::Quadratic {
+                                    from: convert_point(from.into()),
+                                    ctrl: convert_point(ctrl.into()),
+                                    to: convert_point(to.into()),
+                                }
+                            }
+                            bezier_rs::BezierHandles::Cubic {
+                                handle_start: ctrl1,
+                                handle_end: ctrl2,
+                            } => lyon::path::PathEvent::Cubic {
+                                from: convert_point(from.into()),
+                                ctrl1: convert_point(ctrl1.into()),
+                                ctrl2: convert_point(ctrl2.into()),
+                                to: convert_point(to.into()),
+                            },
+                        }
+                    },
+                ))
+                .chain(std::iter::once(lyon::path::PathEvent::End {
+                    last: end_point,
+                    first: begin_point,
+                    close: subpath.closed,
+                }))
+                .collect_vec()
+        }))
     }
 }
 
-impl From<bezier_rs::Subpath<ManipulatorGroupId>> for Subpaths {
-    #[inline]
-    fn from(subpath: bezier_rs::Subpath<ManipulatorGroupId>) -> Self {
-        Self(vec![subpath])
+impl FromIterator<bezier_rs::Subpath<ManipulatorGroupId>> for Path {
+    fn from_iter<T: IntoIterator<Item = bezier_rs::Subpath<ManipulatorGroupId>>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+pub struct PathBuilder {
+    builder: lyon::path::Builder,
+    closed: bool,
+}
+
+impl PathBuilder {
+    pub fn new() -> Self {
+        Self {
+            builder: lyon::path::Builder::new(),
+            closed: true,
+        }
+    }
+
+    pub fn build(mut self) -> Path {
+        if !self.closed {
+            self.builder.end(false);
+        }
+        Path::from_lyon_path(&self.builder.build())
+    }
+}
+
+impl ttf_parser::OutlineBuilder for PathBuilder {
+    fn move_to(&mut self, x: f32, y: f32) {
+        if !self.closed {
+            self.builder.end(false);
+        }
+        self.closed = false;
+        self.builder.begin(lyon::geom::point(x, y));
+    }
+
+    fn line_to(&mut self, x: f32, y: f32) {
+        self.closed = false;
+        self.builder.line_to(lyon::geom::point(x, y));
+    }
+
+    fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
+        self.closed = false;
+        self.builder
+            .quadratic_bezier_to(lyon::geom::point(x1, y1), lyon::geom::point(x, y));
+    }
+
+    fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
+        self.closed = false;
+        self.builder.cubic_bezier_to(
+            lyon::geom::point(x1, y1),
+            lyon::geom::point(x2, y2),
+            lyon::geom::point(x, y),
+        );
+    }
+
+    fn close(&mut self) {
+        self.closed = true;
+        self.builder.end(true);
     }
 }
 
